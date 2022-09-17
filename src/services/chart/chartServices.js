a048fdd8124d5a4f154852236ba43ef70eb702e0
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2022-09-18 03:40:11 +0530

    added services

---
+++ src/services/chart/chartServices.js
@@ -0,0 +1,16 @@
+import data2018 from "../../data/2018.json";
+import data2019 from "../../data/2019.json";
+import data2020 from "../../data/2020.json";
+
+export const getChartDataByYear = async year => {
+  switch (year) {
+    case 2018:
+      return data2018;
+    case 2019:
+      return data2019;
+    case 2020:
+      return data2020;
+    default:
+      return "No data found";
+  }
+};
---
+++ src/services/index.js
@@ -0,0 +1,1 @@
+export { getChartDataByYear } from "./chart/chartServices";
